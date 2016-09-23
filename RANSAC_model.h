f9b4e9ffb94f6495ea50a587fc4b18fd6b737de6
tesyolan <tesfa1993@yahoo.com>
2016-09-23 12:21:05 +0300

    Fix scale and rotation to utilize 4 random points for better results.

--- RANSAC_model.h
+++ RANSAC_model.h
@@ -83,69 +83,120 @@ public:
         m_model = std::make_pair(model,votes);
         return;
     }
+//
+//    Point2f h = -(m_points_normalized[Point2->m_class] - m_points_normalized[Point1->m_class]);
+    bool estimate_rotation = true;
+    bool estimate_scale = true;
+    vector<float> changes_scale;
+    if (estimate_scale) changes_scale.reserve(InputParams.size()*InputParams.size());
+    vector<float> changes_angles;
+    if (estimate_rotation) changes_angles.reserve(InputParams.size()*InputParams.size());
 
-    Point2f h = -(m_points_normalized[Point2->m_class] - m_points_normalized[Point1->m_class]);
+    for (size_t i = 0; i < InputParams.size(); i++)
+    {
+    auto Point1 = std::dynamic_pointer_cast<Point2D>(InputParams[i]);
 
+        for (size_t j = 0; j < InputParams.size(); j++)
+        {
+        auto Point2 = std::dynamic_pointer_cast<Point2D>(InputParams[j]);
+            if (Point2->m_class != Point1->m_class)
+            {
+                Point2f v = Point2->m_normalized - Point1->m_normalized;
+                Point2f h = (m_points_normalized[Point2->m_class] - m_points_normalized[Point1->m_class]);
+                if (estimate_scale)
+                {
+                    float distance = norm(v);
+                    float distance_original = norm(h);
+                    float change_scale = distance / distance_original;
+                    changes_scale.push_back(change_scale);
+                }
+
+                if (estimate_rotation)
+                {
+                    float angle = atan2(v.y,v.x);
+                    float angle_original = atan2(h.y,h.x);
+                    float change_angle = angle - angle_original;
+
+                    //Fix long way angles
+                    if (fabs(change_angle) > M_PI) {
+                        change_angle = sgn(change_angle) * 2 * M_PI + change_angle;
+                    }
+
+                    changes_angles.push_back(change_angle);
+                }
+            }
+
+        }
+
+    }
+
+    //Do not use changes_scale, changes_angle after this point as their order is changed by median()
+    if (changes_scale.size() < 2) scale = 1;
+    else scale = median(changes_scale);
+
+    if (changes_angles.size() < 2) rotation = 0;
+    else rotation = median(changes_angles);
     //square root comptation
-    GRANSAC::VPFloat pol_a = h.y * v.x - h.x*v.y;
-    GRANSAC::VPFloat pol_b = -2*(h.x*v.x + h.y*v.y );
-    GRANSAC::VPFloat pol_c = (h.x*v.y) + (h.y*v.x);
-
-    GRANSAC::VPFloat determinant = pol_b*pol_b - 4*pol_a*pol_c;
-
-    GRANSAC::VPFloat x1, x2;
-    GRANSAC::VPFloat alpha_1, alpha_2, s_1, s_2;
-
-    if (determinant > 0)
-    {
-        x1 = (-pol_b + sqrt(determinant))/(2*pol_a);
-        x2 = (-pol_b - sqrt(determinant))/(2*pol_a);
-    }
-    else if(determinant == 0)
-    {
-        x1 = (-pol_b + sqrt(determinant))/(2*pol_a);
-        x2 = x1;
-    }
-    else
-    {
-        m_model = std::make_pair(model,votes);
-        return;
-    }
-
-    alpha_1 = 2*atan(x1); //Is this equivalent to the area that is mentioned earlier.
-    alpha_2 = 2*atan(x2);
-    s_1 = (v.x + (v.x*(x1*x1))) / (h.x - h.x*(x1*x1) - h.y*2*x1) ;
-    s_2 = (v.x + (v.x*(x2*x2))) / (h.x - h.x*(x2*x2) - h.y*2*x2) ;
-
-    if ( s_1 == s_2 )
-    {
-        if (s_1 < 0)
-        {
-            scale = -s_1;
-            rotation = 3.14159265;
-        }
-        else
-        {
-            scale = s_1;
-            rotation = alpha_1;
-        }
-    }
-    else
-    {
-    if (s_1 < 0)
-    {
-        scale = s_2;
-        rotation = alpha_2;
-    }
-    else
-    {
-        scale = s_1;
-        rotation = alpha_1;
-    }
-    }
+//    GRANSAC::VPFloat pol_a = h.y * v.x - h.x*v.y;
+//    GRANSAC::VPFloat pol_b = -2*(h.x*v.x + h.y*v.y );
+//    GRANSAC::VPFloat pol_c = (h.x*v.y) + (h.y*v.x);
+//
+//    GRANSAC::VPFloat determinant = pol_b*pol_b - 4*pol_a*pol_c;
+//
+//    GRANSAC::VPFloat x1, x2;
+//    GRANSAC::VPFloat alpha_1, alpha_2, s_1, s_2;
+//
+//    if (determinant > 0)
+//    {
+//        x1 = (-pol_b + sqrt(determinant))/(2*pol_a);
+//        x2 = (-pol_b - sqrt(determinant))/(2*pol_a);
+//    }
+//    else if(determinant == 0)
+//    {
+//        x1 = (-pol_b + sqrt(determinant))/(2*pol_a);
+//        x2 = x1;
+//    }
+//    else
+//    {
+//        m_model = std::make_pair(model,votes);
+//        return;
+//    }
+//
+//    alpha_1 = 2*atan(x1); //Is this equivalent to the area that is mentioned earlier.
+//    alpha_2 = 2*atan(x2);
+//    s_1 = (v.x + (v.x*(x1*x1))) / (h.x - h.x*(x1*x1) - h.y*2*x1) ;
+//    s_2 = (v.x + (v.x*(x2*x2))) / (h.x - h.x*(x2*x2) - h.y*2*x2) ;
+//
+//    if ( s_1 == s_2 )
+//    {
+//        if (s_1 < 0)
+//        {
+//            scale = -s_1;
+//            rotation = 3.14159265;
+//        }
+//        else
+//        {
+//            scale = s_1;
+//            rotation = alpha_1;
+//        }
+//    }
+//    else
+//    {
+//    if (s_1 < 0)
+//    {
+//        scale = s_2;
+//        rotation = alpha_2;
+//    }
+//    else
+//    {
+//        scale = s_1;
+//        rotation = alpha_1;
+//    }
+//    }
     model.push_back(scale);
     model.push_back(rotation);
 
+//    auto Point1 = std::dynamic_pointer_cast<Point2D>(InputParams[0]);
     votes = Point1->m_normalized - model[0] * rotate(m_points_normalized[Point1->m_class], model[1]);
 
     m_model = std::make_pair(model,votes);
